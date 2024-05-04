745c3745d0bcae976dc9f2cdd686b5c6f60d18d2
ForSubmissionOnly <niusha@princeton.edu>
2024-05-04 00:14:46 -0400

    tensor size

--- run_glue.py
+++ run_glue.py
@@ -154,17 +154,17 @@ def train(args, train_dataset, model, tokenizer):
 
             torch.distributed.barrier()
             start_comm_time = time.time()
+            # print the tensor size and type
+            print("----size and type of the tensor")
+            print(model.parameters().size())
+            print(model.parameters().dtype)
+            print("size and type of the tensor----")
+            print("----size and type of the QUANTIZED tensor")
+            print(quantize(param.grad.data).type(torch.bool).size())
+            print(quantize(param.grad.data).type(torch.bool).dtype)
+            print("size and type of the QUANTIZED tensor----")
             # Gradient synchronization
             for i, param in enumerate(model.parameters()):
-                # print the tensor size and type
-                print("----size and type of the tensor")
-                print(param.grad.data.size())
-                print(param.grad.data.dtype)
-                print("size and type of the tensor----")
-                print("----size and type of the QUANTIZED tensor")
-                print(quantize(param.grad.data).size())
-                print(quantize(param.grad.data).dtype)
-                print("size and type of the QUANTIZED tensor----")
                 torch.distributed.all_reduce(quantize(param.grad.data), op=torch.distributed.ReduceOp.SUM)
                 
             torch.distributed.barrier()  # Make sure all processes have received averaged gradients before continuing
